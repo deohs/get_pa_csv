6b97daf68fd79f9396a10b729a239cf13ac83b4e
Brian High <high@uw.edu>
2023-08-15 08:55:08 -0700

    Minor code cleanup.

--- read_data.R
+++ read_data.R
@@ -5,11 +5,16 @@
 if (!requireNamespace("pacman", quietly = TRUE)) install.packages('pacman')
 pacman::p_load(here, dplyr, tidyr, readr, purrr, ggplot2)
 
-# Define path to data files
+# Initialize variables
 data_dir <- here("data")
+data_csv_file_suffix <- "_pm25.csv"
+cbPalette <- c("#56B4E9", "#E69F00", "#CC79A7")
+images_dir <- here("images")
+plot_filepath <- here(images_dir, "pa_data.png")
 
 # Import CSV files into a single data frame, sort and remove duplicates
-fp <- list.files(data_dir, "*_pm25.csv", full.names = TRUE, recursive = TRUE)
+fp <- list.files(data_dir, data_csv_file_suffix, 
+                 full.names = TRUE, recursive = TRUE)
 df <- map_df(fp, read_csv, col_types = 'Tcnnnn', show_col_types = FALSE,
              locale = locale(tz = Sys.timezone())) %>% 
   arrange(sensor_index, time_stamp) %>% 
@@ -22,7 +27,6 @@ df_long <- df %>%
                names_to = "variable", values_to = "value")
 
 # Plot data
-cbPalette <- c("#56B4E9", "#E69F00", "#CC79A7")
 g <- ggplot(df_long, aes(time_stamp, value, color = variable)) + 
   geom_line(linewidth = 0.5, alpha = 0.7) + 
   facet_wrap(. ~ sensor_index, nrow = 3) + 
@@ -30,8 +34,7 @@ g <- ggplot(df_long, aes(time_stamp, value, color = variable)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 # Create images folder
-images_dir <- here("images")
 dir.create(images_dir, showWarnings = FALSE, recursive = TRUE)
 
 # Save plot
-ggsave(here(images_dir, "pa_data.png"), plot = g, width = 8, height = 5)
+ggsave(plot_filepath, plot = g, width = 8, height = 5)
